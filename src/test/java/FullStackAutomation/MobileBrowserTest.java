c8e61ea3ebb9eaa54b5b58afc81de9d8c1fe8177
vijay <merlintomb@gmail.com>
2025-04-14 00:49:39 +0530

    first commit

---
+++ src/test/java/FullStackAutomation/MobileBrowserTest.java
@@ -0,0 +1,17 @@
+package FullStackAutomation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+public class MobileBrowserTest  extends BaseTest_MobileBrowser{
+
+    /*@Test
+    public void launchMobileBrowserOnApp() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.get("https://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("appium");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+    }*/
+    // boave code will not work as one the emulator chrome is not getting identified
+}
